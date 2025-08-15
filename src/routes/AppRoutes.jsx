3ab94a5dbbb1b3ec7d87c18e59d416af3483fa99
Luan Guimarães <luangmrs@gmail.com>
2025-08-15 11:35:14 -0300

    fix: rota padrão

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,11 +3,13 @@ import LoginPage from "../components/LoginPage/LoginPage.jsx";
 import Home from "../components/FeedPage/Home.jsx";
 import PrivateRoutes from "../components/PrivateRoutes.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route
           path="/home"
